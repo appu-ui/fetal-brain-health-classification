fae0ae1b8aa8ae127b77cdd81f7a4be0bddeada5
appu-ui <1ms22ci014@msrit.edu>
2025-05-14 23:24:24 +0530

    Initial commit

--- app.py
+++ app.py
@@ -1,97 +1,65 @@
 import os
-import sys
-
-# Flask
-from flask import Flask, redirect, url_for, request, render_template, Response, jsonify, redirect
+from flask import Flask, request, render_template, jsonify
 from werkzeug.utils import secure_filename
 from gevent.pywsgi import WSGIServer
 
-# TensorFlow and tf.keras
-import tensorflow as tf
-from tensorflow import keras
-
-from tensorflow.keras.applications.imagenet_utils import preprocess_input, decode_predictions
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
-
-# Some utilites
 import numpy as np
-from util import base64_to_pil
+from PIL import Image
+from tensorflow.keras.models import load_model
 
+# Your class names in the order your model was trained on
+class_names = [
+    "anold-chiari-malformation",
+    "arachnoid-cyst",
+    "cerebellah-hypoplasia",
+    "colphocephaly",
+    "encephalocele",
+    "holoprosencephaly",
+    "hydracenphaly",
+    "intracranial-hemorrdge",
+    "intracranial-tumor",
+    "m-magna",
+    "mild-ventriculomegaly",
+    "moderate-ventriculomegaly",
+    "normal",
+    "polencephaly",
+    "severe-ventriculomegaly",
+    "vein-of-galen"
+] # <-- Replace with your actual class names
 
-# Declare a flask app
+# Flask app
 app = Flask(__name__)
 
-
-# You can use pretrained model from Keras
-# Check https://keras.io/applications/
-# or https://www.tensorflow.org/api_docs/python/tf/keras/applications
-
-from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2
-model = MobileNetV2(weights='imagenet')
-
+# Load your trained model
+MODEL_PATH = 'models/best_model.keras'  # or 'models/model.h5'
+model = load_model(MODEL_PATH)
 print('Model loaded. Check http://127.0.0.1:5000/')
 
-
-# Model saved with Keras model.save()
-MODEL_PATH = 'models/your_model.h5'
-
-# Load your own trained model
-# model = load_model(MODEL_PATH)
-# model._make_predict_function()          # Necessary
-# print('Model loaded. Start serving...')
-
-
 def model_predict(img, model):
-    img = img.resize((224, 224))
-
-    # Preprocessing the image
-    x = image.img_to_array(img)
-    # x = np.true_divide(x, 255)
-    x = np.expand_dims(x, axis=0)
-
-    # Be careful how your trained model deals with the input
-    # otherwise, it won't make correct prediction!
-    x = preprocess_input(x, mode='tf')
-
+    img = img.convert('L')  # Convert to grayscale
+    img = img.resize((64, 64))
+    x = np.array(img) / 255.0
+    x = np.expand_dims(x, axis=-1)  # (64, 64, 1)
+    x = np.expand_dims(x, axis=0)   # (1, 64, 64, 1)
     preds = model.predict(x)
-    return preds
-
+    pred_idx = np.argmax(preds)
+    pred_class = class_names[pred_idx]
+    pred_proba = float(np.max(preds))
+    return pred_class, pred_proba
 
 @app.route('/', methods=['GET'])
 def index():
-    # Main page
     return render_template('index.html')
 
-
-@app.route('/predict', methods=['GET', 'POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
-    if request.method == 'POST':
-        # Get the image from post request
-        img = base64_to_pil(request.json)
-
-        # Save the image to ./uploads
-        # img.save("./uploads/image.png")
-
-        # Make prediction
-        preds = model_predict(img, model)
-
-        # Process your result for human
-        pred_proba = "{:.3f}".format(np.amax(preds))    # Max probability
-        pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
-
-        result = str(pred_class[0][0][1])               # Convert to string
-        result = result.replace('_', ' ').capitalize()
-        
-        # Serialize the result, you can add additional fields
-        return jsonify(result=result, probability=pred_proba)
-
-    return None
-
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file uploaded'}), 400
+    file = request.files['file']
+    img = Image.open(file.stream)
+    pred_class, pred_proba = model_predict(img, model)
+    return jsonify(result=pred_class, probability=round(pred_proba, 3))
 
 if __name__ == '__main__':
-    # app.run(port=5002, threaded=False)
-
-    # Serve the app with gevent
     http_server = WSGIServer(('0.0.0.0', 5000), app)
     http_server.serve_forever()
